307da91d000f16cc77085c83983eada7a27c6e54
Jesper Andersen <jespera@localhost.localdomain>
2012-09-21 00:31:42 +0200

    Implement more of the Game signature
    Remaining work:
    - init-board
    - collection of clusters to score&clear
    - make cells drop

--- hexic.ml
+++ hexic.ml
@@ -1,6 +1,6 @@
 open Game
 
-module Game : Game =
+module Game =
   struct
     type cell = R | G | B | Y | E
 
@@ -8,12 +8,21 @@ module Game : Game =
     type board = cell list list
 
     type board_coord = int * int
+
+    (* assumption: coordinates come in _clockwise_ order *)
     type position = board_coord * board_coord * board_coord 
+
+    type cluster = board_coord list
+
+    let cluster_eq cl1 cl2 = 
+      List.for_all (fun x -> List.mem x cl2) cl1 &&
+      List.for_all (fun x -> List.mem x cl1) cl2
 
     let row_width = 5
     let num_rows = 17
     (* 5 * 17 = 85 cells *)
 
+    (* TODO *)
     let init_board seed = 
       (* randomly generate board from given seed *)
       [ [R;R;R];
@@ -22,17 +31,83 @@ module Game : Game =
        [G;G;G]
       ]
 
+    (* TODO *)
     let get_rotatable_positions board = 
       (* based on length and heigth of board, systematically construct all
        * rotatable positions
        *)
-      [((0,0),(0,1),(0,2))
+      [((0,1),(0,0),(0,2))
       ]
 
-    let rotate board ((x1,y1), (x2,y2), (x3,y3)) = board
+    let rec set_row row x value =
+      match row with
+      | [] -> failwith ("'row_set' out of bounds " ^ string_of_int x)
+      | e :: row ->
+          if x = 0 then value :: row
+          else e :: set_row row (x - 1) value
 
-    let clear_and_score board = (board, 0)
+    let rec set_board board (x,y) value =
+      (* traverse board-structure until reaching 0,0 *)
+      (* first find the correct row; then colum in that row *)
+      match board with 
+      | [] -> failwith ("'set_bounds' out of bounds " ^ string_of_int y)
+      | row :: board -> 
+          if y = 0 then set_row row x value :: board
+          else row :: set_board board (x, y - 1) value
+    
+    let rec get_row row x =
+      match row with
+      | [] -> failwith ("'row_get' out of bounds " ^ string_of_int x)
+      | e :: row ->
+          if x = 0 then e
+          else get_row row (x - 1)
+
+    let rec get_board board (x, y) =
+      match board with 
+      | [] -> failwith ("'set_bounds' out of bounds " ^ string_of_int y)
+      | row :: board -> 
+          if y = 0 then get_row row x
+          else get_board board (x, y - 1)
+
+    let rotate board (coord1, coord2, coord3) = 
+      let val1 = get_board board coord1
+      and val2 = get_board board coord2
+      and val3 = get_board board coord3
+      in
+      set_board (set_board (set_board board 
+                                      coord1 val2)
+                           coord2 val3)
+                coord3 val1
+
+    (* TODO *)
+    let collect_clusters board = [[]] 
+
+    let rec three_pow n = if n = 1 then 3 else 3 * three_pow (n - 1)
+
+    let score_cluster cluster =
+     (* a cluster of size 3 being cleared provides 3 points, while a cluster of
+      * size 4 provides 3*3 = 9 points and a cluster of
+      * size 5 provides 3*3*3 = 27
+      * ...
+      * size n provides 3^(n-2) 
+      *)
+     three_pow (List.length cluster - 2)
+
+    let clear_cluster board cluster = 
+      List.fold_left (fun clr_board pos -> set_board clr_board pos E) board cluster
+
+    let clear_and_score board =
+      (* summarize each cluster-score *)
+      let clear_clusters = List.fold_left clear_cluster board in
+      let clusters = collect_clusters board in
+      (clear_clusters clusters
+      ,List.fold_left (+) 0 (List.map score_cluster clusters)
+      )
+
+
+    (* TODO *)
     let drop_cells board = board
+
     (* simple version of printing first *)
     let string_of_board board = 
       let string_of_cell cell = 
